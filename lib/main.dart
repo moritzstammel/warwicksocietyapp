38965ff004367f89aab394028e719a61f0c3a3e4
moritzstammel <moritz_paul.stammel@mailbox.tu-dresden.de>
2023-08-12 23:46:57 +0200

    removed unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:warwicksocietyapp/login_screen.dart';
-import 'dynamic_link_service.dart';
 import 'firebase_options.dart';
 import 'main_screen.dart';
 
