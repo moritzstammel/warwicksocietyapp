a3cc553fbc9e81777b89a83134308ea313fa5019
moritzstammel <moritz_paul.stammel@mailbox.tu-dresden.de>
2023-08-28 17:09:23 +0200

    added cloud function on society update chat

--- functions/index.js
+++ functions/index.js
@@ -38,6 +38,62 @@ exports.updateSpotlightsOnSocietyUpdate = functions.firestore
   });
 
 
+  exports.updateChatsOnSocietyUpdate = functions.firestore
+  .document('/universities/university-of-warwick/societies/{societyId}')
+  .onUpdate(async (change, context) => {
+    const updatedSocietyData = change.after.data();
+    const updatedSocietyRef = change.after.ref;
+
+    const updatedLogoUrl = updatedSocietyData.logo_url;
+    const updatedSocietyName = updatedSocietyData.name;
+
+    const chatsCollection = firestore.collection('/universities/university-of-warwick/chats');
+
+    const snapshot = await chatsCollection
+      .where('society.ref', '==', updatedSocietyRef)
+      .get();
+
+    const batch = firestore.batch();
+
+    snapshot.forEach((doc) => {
+      const chatRef = doc.ref;
+      const chatData = doc.data();
+
+      // Update society fields in chat
+      batch.update(chatRef, {
+        'society.logo_url': updatedLogoUrl,
+        'society.name': updatedSocietyName
+      });
+
+      // Update author fields in messages
+      const messages = chatData.messages || [];
+      const updatedMessages = messages.map((message) => {
+        if (message.author && message.author.ref.path === updatedSocietyRef.path) {
+          return {
+            ...message,
+            author: {
+              ...message.author,
+              image_url: updatedLogoUrl,
+              name: updatedSocietyName
+            }
+          };
+        }
+        return message;
+      });
+
+      batch.update(chatRef, { messages: updatedMessages });
+    });
+
+    batch.update(updatedSocietyRef, {
+      logo_url: updatedLogoUrl,
+      name: updatedSocietyName
+    });
+
+    return batch.commit();
+  });
+
+
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
