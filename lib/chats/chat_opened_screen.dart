8f045422f681cf5d596f1004ed56edeb54705003
moritzstammel <moritz_paul.stammel@mailbox.tu-dresden.de>
2023-09-04 11:41:11 +0200

    made the opened chat more beautiful

--- lib/chats/chat_opened_screen.dart
+++ lib/chats/chat_opened_screen.dart
@@ -17,8 +17,10 @@ class ChatOpenedScreen extends StatefulWidget {
   _ChatOpenedScreenState createState() => _ChatOpenedScreenState();
 }
 
-class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
+class _ChatOpenedScreenState extends State<ChatOpenedScreen> with WidgetsBindingObserver {
+  final ScrollController _scrollController = new ScrollController();
   final TextEditingController _messageController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   late Stream<DocumentSnapshot> chatStream;
 
   static const Map<int, String> _weekdayShortMap = {
@@ -31,9 +33,20 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
     7: 'Sun',
   };
 
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+
+
+  }
+
   @override
   void initState() {
     super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
 
     chatStream = FirebaseFirestore.instance
         .collection("universities")
@@ -41,20 +54,41 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
         .collection("chats")
         .doc(widget.chatId)
         .snapshots();
+
+  }
+  @override
+  void didChangeMetrics() {
+    final mediaQuery = MediaQuery.of(context);
+    final isKeyboardOpening = mediaQuery.viewInsets.bottom > 0 && mediaQuery.viewInsets.bottom != mediaQuery.padding.bottom;
+
+
+    if (isKeyboardOpening) {
+
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeOut,
+      );
+    }
   }
 
-  void navigateToEventsDetails(Event event){
-    Navigator.push(context, MaterialPageRoute(builder: (context) => EventDetailsScreen(event: event)));
+
+  void navigateToEventsDetails(Event event) {
+    Navigator.push(context, MaterialPageRoute(
+        builder: (context) => EventDetailsScreen(event: event)));
   }
 
 
   int daysBetween(DateTime from, DateTime to) {
     from = DateTime(from.year, from.month, from.day);
     to = DateTime(to.year, to.month, to.day);
-    return (to.difference(from).inHours / 24).round();
+    return (to
+        .difference(from)
+        .inHours / 24).round();
   }
 
   String formatTime(DateTime dateTime) {
+
     DateTime now = DateTime.now();
     int difference = daysBetween(dateTime, now);
 
@@ -64,25 +98,30 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
     String minute = dateTime.minute.toString().padLeft(2, '0');
     //String period = dateTime.hour < 12 ? 'AM' : 'PM';
     String time = '$hour:$minute';
-
-    if (difference==0)  return time;
+    return time;
+    if (difference == 0) return time;
     if (difference <= 1) return 'yesterday, $time';
     if (difference <= 6) return '${difference} days ago, $time';
     if (difference <= 28) {
       int weeks = (difference / 7).ceil();
-      return '$weeks week${weeks > 1 ? 's' : ''} ago, $time';}
+      return '$weeks week${weeks > 1 ? 's' : ''} ago, $time';
+    }
     if (difference <= 365) {
       int differenceInMonths = (difference / 12).round();
-      return '$differenceInMonths month${differenceInMonths > 1 ? 's' : ''} ago, $time';}
+      return '$differenceInMonths month${differenceInMonths > 1
+          ? 's'
+          : ''} ago, $time';
+    }
 
     int differenceInYears = (difference / 365).round();
-    return '$differenceInYears year${differenceInYears > 1 ? 's' : ''} ago, $time';
-
-
+    return '$differenceInYears year${differenceInYears > 1
+        ? 's'
+        : ''} ago, $time';
   }
 
   @override
   Widget build(BuildContext context) {
+
     return StreamBuilder<DocumentSnapshot>(
       stream: chatStream,
       builder: (context, snapshot) {
@@ -101,6 +140,7 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
             snapshot.data!.data() as Map<String, dynamic>, widget.chatId);
 
         return Scaffold(
+
           backgroundColor: Colors.white,
           appBar: AppBar(
             backgroundColor: Colors.white,
@@ -152,9 +192,11 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
               IconButton(
                 icon: Icon(Icons.info_outline, color: Colors.black),
                 onPressed: () async {
-                  DocumentReference eventsRef = FirebaseFirestore.instance.doc("universities/university-of-warwick/events/${chat.id}");
+                  DocumentReference eventsRef = FirebaseFirestore.instance.doc(
+                      "universities/university-of-warwick/events/${chat.id}");
                   final eventData = await eventsRef.get();
-                  Event event = Event.fromJson(eventData.data() as Map<String,dynamic>, eventData.id);
+                  Event event = Event.fromJson(
+                      eventData.data() as Map<String, dynamic>, eventData.id);
                   navigateToEventsDetails(event);
                 },
               ),
@@ -163,135 +205,205 @@ class _ChatOpenedScreenState extends State<ChatOpenedScreen> {
           body: Column(
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: chat.messages.length,
-                  itemBuilder: (context, index) {
-                    Message message = chat.messages[index];
-                    bool isUserMessage =
-                        message.author.ref.id == widget.user.id;
+                  child: ListView.builder(
+                    controller: _scrollController,
+                    itemCount: chat.messages.length,
+                    itemBuilder: (context, index) {
+                      Message message = chat.messages[index];
+                      bool isUserMessage = message.author.ref.id ==
+                          widget.user.id;
 
-                    return Column(
-                      crossAxisAlignment: isUserMessage
-                          ? CrossAxisAlignment.end
-                          : CrossAxisAlignment.start,
-                      children: [
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        child: Row(
 
-                        Container(
-                          margin : EdgeInsets.symmetric(horizontal: 8),
-                          child: Text(
-                            formatTime(message.createdAt),
-                            style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontSize: 12,
-                              color:
-                              isUserMessage ? Colors.black : Colors.black,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Container(
-                          margin: EdgeInsets.all(8),
-                          padding: EdgeInsets.all(8),
-                          constraints: BoxConstraints(
-                            maxWidth: 250,
-                          ),
-                          decoration: BoxDecoration(
-                            color: isUserMessage
-                                ? Colors.black
-                                : Color(0xFFF7F7F7),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: isUserMessage
-                                ? CrossAxisAlignment.end
-                                : CrossAxisAlignment.start,
+                            mainAxisAlignment: isUserMessage ? MainAxisAlignment
+                                .end : MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
                               if (!isUserMessage)
-                                CircleAvatar(
-                                  radius: 18,
-                                  backgroundImage:
-                                  NetworkImage(message.author.imageUrl),
-                                  backgroundColor: Colors.white,
+                                Container(
+                                  width: 36,
+                                  height: 36,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                          chat.societyInfo.logoUrl),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
                                 ),
-                              Text(
-                                message.content,
-                                style: TextStyle(
-                                  fontFamily: 'Inter',
-                                  fontSize: 16,
-                                  color:
-                                  isUserMessage ? Colors.white : Colors.black,
-                                ),
+                              SizedBox(width: 8,),
+                              Column(
+                                crossAxisAlignment: isUserMessage
+                                    ? CrossAxisAlignment.end
+                                    : CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 12),
+                                    decoration: BoxDecoration(
+                                      color: isUserMessage
+                                          ? Colors.black
+                                          : Color(0xFFF7F7F7),
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    constraints: BoxConstraints(
+                                      maxWidth: 250,
+                                    ),
+                                    child: Text(
+                                      message.content,
+                                      style: TextStyle(
+                                        fontFamily: 'Inter',
+                                        fontSize: 16,
+                                        color: isUserMessage
+                                            ? Colors.white
+                                            : Colors.black,
+                                      ),
+
+                                    ),
+                                  ),
+                                  SizedBox(height: 4,),
+                                  Row(
+                                    children: [
+                                      if (!isUserMessage)
+                                        Text(
+                                          message.author.name, style: TextStyle(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.black
+                                        ),),
+                                      if (!isUserMessage) SizedBox(width: 2,),
+                                      Text("sent at ${formatTime(
+                                          message.createdAt)}",
+                                        style: TextStyle(
+                                            fontSize: 10,
+                                            color: Color(0xFF888888)
+                                        ),)
+
+                                    ],
+                                  )
+                                ],
                               ),
-                            ],
-                          ),
+                            ]
                         ),
-                      ],
-                    );
-                  },
-                ),
+                      );
+                    },
+                  )
+
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _messageController,
-                        decoration: InputDecoration(
-                          hintText: "Type your message...",
+                     Container(
+
+                        height: 56,
+                        width: 280,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF7F7F7),
+                          borderRadius: BorderRadius.circular(28),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  focusNode: _focusNode,
+                                  controller: _messageController,
+                                  decoration: InputDecoration(
+                                    hintText: "Type your message here...",
+                                    hintStyle: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 16,
+                                      color: Color(0xFF777777),
+                                    ),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    SizedBox(width: 16,),
+                    GestureDetector(
+                      onTap: () => sendMessage(chat),
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Icon(
+
+                            Icons.send,
+                            color: Colors.white,
+                            size: 24,
+                          ),
                         ),
                       ),
                     ),
-                    IconButton(
-                      icon: Icon(Icons.send),
-                      onPressed: () async {
-                        final content = _messageController.text;
-                        if (content.trim().isEmpty) return;
-
-                        Timestamp now = Timestamp.now();
-
-                        final chatRef = FirebaseFirestore.instance
-                            .collection('universities')
-                            .doc('university-of-warwick')
-                            .collection('chats')
-                            .doc(chat.id);
-
-                        await chatRef.update({
-                          'messages': FieldValue.arrayUnion([
-                            {
-                              'author': {
-                                'name': widget.user.username,
-                                'image_url': widget.user.imageUrl,
-                                'ref': FirebaseFirestore.instance
-                                    .doc(
-                                    "/universities/university-of-warwick/users/${widget.user.id}")
-                              },
-                              'content': content,
-                              'is_deleted': false,
-                              'created_at': now
-                            }
-                          ]),
-                          'time_of_last_message': now
-                        });
-
-                        _messageController.clear();
-                      },
-                    ),
                   ],
                 ),
-              ),
+              )
+
             ],
           ),
         );
       },
     );
   }
-}
 
-String formatContent(String content) {
-  if (content.length > 60) {
-    return '${content.substring(0, 60)}...';
-  } else {
-    return content;
+
+  void sendMessage(Chat chat) async {
+    final content = _messageController.text;
+    if (content
+        .trim()
+        .isEmpty) return;
+
+    Timestamp now = Timestamp.now();
+
+    final chatRef = FirebaseFirestore.instance
+        .collection('universities')
+        .doc('university-of-warwick')
+        .collection('chats')
+        .doc(chat.id);
+
+    await chatRef.update({
+      'messages': FieldValue.arrayUnion([
+        {
+          'author': {
+            'name': widget.user.username,
+            'image_url': widget.user.imageUrl,
+            'ref': FirebaseFirestore.instance
+                .doc(
+                "/universities/university-of-warwick/users/${widget.user.id}")
+          },
+          'content': content,
+          'is_deleted': false,
+          'created_at': now
+        }
+      ]),
+      'time_of_last_message': now
+    });
+
+    _messageController.clear();
+
+  }
+
+  String formatContent(String content) {
+    if (content.length > 60) {
+      return '${content.substring(0, 60)}...';
+    } else {
+      return content;
+    }
   }
 }
