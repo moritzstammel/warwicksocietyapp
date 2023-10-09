7a8bc1091fc11e85e6693c19a32a9be172b053ce
moritzstammel <moritz_paul.stammel@mailbox.tu-dresden.de>
2023-10-09 17:58:28 +0200

    refactored spotlight card

--- lib/widgets/spotlight_card.dart
+++ lib/widgets/spotlight_card.dart
@@ -1,11 +1,12 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:warwicksocietyapp/home/spotlight_details.dart';
 import 'package:warwicksocietyapp/spotlight_creation/spotlight_creation_screen.dart';
 import 'package:warwicksocietyapp/widgets/small_society_card.dart';
 import 'dart:async';
-
-import '../models/society_info.dart';
 import '../models/spotlight.dart';
 
 class SpotlightCard extends StatefulWidget {
@@ -19,17 +20,18 @@ class SpotlightCard extends StatefulWidget {
 }
 
 class _SpotlightCardState extends State<SpotlightCard> {
-  int _currentIndex = 0;
   late Timer _timer;
+  late final PageController _pageController;
+  int _currentPage = 0;
 
   @override
   void initState() {
     super.initState();
+    _pageController = PageController(initialPage: Random().nextInt(widget.spotlights.length));
+
     if(widget.spotlights.length <= 1) return;
-    _timer = Timer.periodic(Duration(seconds: 10), (timer) {
-      setState(() {
-        _currentIndex = (_currentIndex + 1) % widget.spotlights.length;
-      });
+    _timer = Timer.periodic(Duration(seconds: 8), (timer) {
+      _pageController.animateToPage((_currentPage + 1), duration: Duration(milliseconds: 250), curve: Curves.easeOut);
     });
   }
 
@@ -37,6 +39,13 @@ class _SpotlightCardState extends State<SpotlightCard> {
   void dispose() {
     if(widget.spotlights.length > 1) _timer.cancel();
     super.dispose();
+  }
+  void resetTimer(){
+    if(widget.spotlights.length <= 1) return;
+    _timer.cancel();
+    _timer = Timer.periodic(Duration(seconds: 8), (timer) {
+      _pageController.animateToPage((_currentPage + 1), duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    });
   }
 
 
@@ -65,131 +74,142 @@ class _SpotlightCardState extends State<SpotlightCard> {
 
   @override
   Widget build(BuildContext context) {
-    if(_currentIndex >= widget.spotlights.length) _currentIndex = 0;
-
-    
-    
       return Container(
         width: double.infinity,
-        child: GestureDetector(
-          onTap: widget.clickable ? () {
-            if(widget.editable) {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SpotlightCreationScreen(spotlight: widget.spotlights[_currentIndex],)),);
-                  return;
-            }
-
+        height: 175,
+        child: PageView.builder(
+          onPageChanged: (index) {
+            resetTimer();
             setState(() {
-              if(widget.spotlights.length>1) {
-                _currentIndex = (_currentIndex + 1) % widget.spotlights.length;
-              }
-              if(widget.spotlights.length==1){
-                openSpotlightInfo(widget.spotlights.first);
-              }
+              _currentPage = index;
             });
-          } : null,
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            padding: EdgeInsets.only(right: 8,top: 8,left: 15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: widget.spotlights[_currentIndex].image ?? NetworkImage( widget.spotlights[_currentIndex].imageUrl),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5),
-                  BlendMode.darken,
-                ),
-              ),
-            ),
-            height: 150,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: widget.editable ? GestureDetector(
-                      onTap: (){
-                        Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SpotlightCreationScreen(spotlight: widget.spotlights[_currentIndex],)),
-                      );},
-                      child: Icon(Icons.edit,color: Colors.white,size: 20,))
-                      : SmallSocietyCard(society: widget.spotlights[_currentIndex].society,)
-                ),
+          },
+          scrollDirection: Axis.horizontal,
+          controller: _pageController,
+          itemBuilder: (context,index){
+            final currentSpotlightIndex = index % widget.spotlights.length;
+            return Container(
 
+              child: GestureDetector(
+                onTap: widget.clickable ? () {
+                  if(widget.editable) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SpotlightCreationScreen(spotlight: widget.spotlights[currentSpotlightIndex],)),);
+                    return;
+                  }
+                  openSpotlightInfo(widget.spotlights[currentSpotlightIndex]);
+
+
+                } : null,
+                child:
                 Container(
-                  width: 150,
-                  child: Column(
-
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  padding: EdgeInsets.only(right: 8,top: 8,left: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: widget.spotlights[currentSpotlightIndex].image ?? NetworkImage( widget.spotlights[currentSpotlightIndex].imageUrl),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5),
+                        BlendMode.darken,
+                      ),
+                    ),
+                  ),
+                  height: 150,
+                  child: Stack(
                     children: [
-                      SizedBox(height: 25,),
-                      Text(
+                      Positioned(
+                          top: 0,
+                          right: 0,
+                          child: widget.editable ? GestureDetector(
+                              onTap: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => SpotlightCreationScreen(spotlight: widget.spotlights[currentSpotlightIndex],)),
+                                );},
+                              child: Icon(Icons.edit,color: Colors.white,size: 20,))
+                              : SmallSocietyCard(society: widget.spotlights[currentSpotlightIndex].society,)
+                      ),
 
-                        widget.spotlights[_currentIndex].title,
-                        maxLines: 2,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Spacer(),
                       Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: ElevatedButton(
-                          onPressed: widget.clickable ? () {
-                            openSpotlightInfo( widget.spotlights[_currentIndex]);
-                          }: (){},
-                          child: Text(
-                            'More Info',
-                            style: TextStyle(color: Colors.black,fontFamily: 'Inter'),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100),
+                        width: 150,
+                        child: Column(
+
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 25,),
+                            Text(
+
+                              widget.spotlights[currentSpotlightIndex].title,
+                              maxLines: 2,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                            minimumSize: Size(100, 33),
-                          ),
+                            Spacer(),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 10),
+                              child: ElevatedButton(
+                                onPressed: widget.clickable ? () {
+                                  openSpotlightInfo( widget.spotlights[currentSpotlightIndex]);
+                                }: (){},
+                                child: Text(
+                                  'More Info',
+                                  style: TextStyle(color: Colors.black,fontFamily: 'Inter'),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.white,
+                                  onPrimary: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  minimumSize: Size(100, 33),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
+
+                      // Paging Indicator Dots (Hide if only one spotlight)
+                      if (widget.spotlights.length > 1)
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: List.generate(
+                                widget.spotlights.length,
+                                    (i) => Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 4),
+                                  width: 6,
+                                  height: 6,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: i == currentSpotlightIndex
+                                        ? Colors.white
+                                        : Colors.white.withOpacity(0.5),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                     ],
                   ),
                 ),
 
-                // Paging Indicator Dots (Hide if only one spotlight)
-                if (widget.spotlights.length > 1)
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          widget.spotlights.length,
-                              (index) => Container(
-                            margin: EdgeInsets.symmetric(horizontal: 4),
-                            width: 6,
-                            height: 6,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: _currentIndex == index
-                                  ? Colors.white
-                                  : Colors.white.withOpacity(0.5),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
+
+              ),
+            );
+          },
+
         ),
       );
   }
